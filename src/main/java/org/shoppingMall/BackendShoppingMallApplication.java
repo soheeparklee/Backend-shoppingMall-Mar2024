9bdf4a54e5cde7f0b9d835750a8162be3cfe2668
soheeparklee <soheeparklee@gmail.com>
2024-03-22 11:48:38 +0000

    feat: DB settings

---
+++ src/main/java/org/shoppingMall/BackendShoppingMallApplication.java
@@ -0,0 +1,12 @@
+package org.shoppingMall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendShoppingMallApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BackendShoppingMallApplication.class, args);
+    }
+
+}
--- src/main/java/org/shoppingMall/commitTest.java
+++
@@ -1,7 +0,0 @@
-package org.shoppingMall;
-
-public class commitTest {
-    public static void main(String[] args) {
-
-    }
-}
