12d54a014a98cbaaacde7e98efca0d295b5f01b5
soheeparklee <soheeparklee@gmail.com>
2024-03-23 20:00:40 +0000

    cart, cartJpa

---
+++ src/main/java/org/shoppingMall/repository/cart/Cart.java
@@ -0,0 +1,30 @@
+package org.shoppingMall.repository.cart;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.shoppingMall.repository.user.User;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cart")
+public class Cart {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "cart_id")
+    private Integer cartId;
+
+    @ManyToOne
+    @JoinColumn(name = "product_option_id")
+    private ProductOption productOption;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "cart_amount", nullable = false)
+    private Integer cartAmount;
+}
